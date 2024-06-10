0c320900a111389161272b48ac85fbfb688deb8c
AndrewSviatih <fagil.list@gmail.com>
2024-06-10 12:53:06 +0300

    db-create-logs

--- src/core/main.py
+++ src/core/main.py
@@ -1,7 +1,7 @@
 from weather.client import WeatherClient
 
-
 async def main():
     async with WeatherClient() as weather:
         await weather.get_temperature('St. Petersburg')
+        print(weather)
         await weather.post_temperature('St. Petersburg', {'hui': 'pizda'})
